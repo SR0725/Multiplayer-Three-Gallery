bef982c18a4ea3c8c6a2d0d9efab7c0d84f25a5a
Ray <62081739+SR0725@users.noreply.github.com>
2024-09-20 14:41:59 +0800

    feat: api

--- apps/web/src/utils/socket.ts
+++ apps/web/src/utils/socket.ts
@@ -2,6 +2,6 @@ import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NEXT_PUBLIC_BACKEND_URL ?? 'https://api.meta-gallery.art/';
+  process.env.NEXT_PUBLIC_BACKEND_URL ?? 'https://gallery-api.zeabur.app/';
 
 export const socket = io(URL);
